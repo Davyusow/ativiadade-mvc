661b678e1992b7046f67c0d35476d6bcfc8779bc
Matheus Aroxa <matheusaroxa13@gmail.com>
2025-05-24 16:42:06 -0300

    controller

--- src/Controllers/TaskController.java
+++ src/Controllers/TaskController.java
@@ -1,5 +1,27 @@
 package Controllers;
 
+import models.Task;
+import repository.TaskRepository;
+
+import java.util.List;
+
 public class TaskController {
 
+    private TaskRepository repository;
+
+    public TaskController(TaskRepository repository) {
+        this.repository = repository;
+    }
+
+    public void createTask(Task task) {
+        repository.addTask(task);
+    }
+
+    public List<Task> getAllTasks() {
+        return repository.listTasks();
+    }
+
+    public void markAsDone(String title) {
+        //falta implementar
+    }
 }
